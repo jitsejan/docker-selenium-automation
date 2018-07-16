0b0dac06611a93be7c1ffbc688cdc7d1f9048ed0
Jitse-Jan <code@jitsejan.com>
2018-07-16 15:11:57 +0100

    Added screenshot with Selenium.

--- nintendoscreenshot/nintendoscreenshot/spiders/nintendomain.py
+++ nintendoscreenshot/nintendoscreenshot/spiders/nintendomain.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 BASE_URL = 'http://www.nintendo.com/'
 
@@ -8,7 +10,16 @@ class NintendomainSpider(scrapy.Spider):
     allowed_domains = ['www.nintendo.com']
     start_urls = [BASE_URL]
 
+    def __init__(self, *args, **kwargs):
+        super(NintendomainSpider, self).__init__(*args, **kwargs)
+        self.driver = webdriver.Remote("http://localhost:4444/wd/hub", DesiredCapabilities.CHROME)
+        self.driver.get(BASE_URL)
+        self.driver.get_screenshot_as_file('screenshot.png')
+
     def parse(self, response):
+        driver = webdriver.Remote("http://localhost:4444/wd/hub", DesiredCapabilities.CHROME)
+        driver.get('http://prod.jitsejan.com:8080')
+        driver.save_screenshot('vue_frontpage.png')
         for elem in response.css('section.new-releases li a'):
             yield {
                'name': elem.css('div[itemprop="name"]::text').extract_first(),
@@ -16,3 +27,6 @@ class NintendomainSpider(scrapy.Spider):
                'link': BASE_URL + elem.xpath('@href').extract_first(),
                'image': elem.css('img::attr(src)').extract_first()
             }
+
+    def closed(self, reason):
+        self.driver.quit()
